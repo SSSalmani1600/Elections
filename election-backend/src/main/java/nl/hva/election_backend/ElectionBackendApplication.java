4e3c221c2381ddc67708fc2393614db1858c240a
Afnaan Makhloufi <afnaan.makhloufi@hva.nl>
2025-09-12 12:33:30 +0200

    backend added

---
+++ election-backend/src/main/java/nl/hva/election_backend/ElectionBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.hva.election_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectionBackendApplication.class, args);
+	}
+
+}
---
+++ election-backend/src/test/java/nl/hva/election_backend/ElectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hva.election_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
