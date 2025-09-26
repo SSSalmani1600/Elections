22f26b3226986d24461abe92106e23f09b414025
Daan <daan.hoedjes@hva.nl>
2025-09-26 15:52:36 +0200

    add LoginView.vue

---
+++ election-frontend/src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bg-white p-4 flex flex-col justify-around">
+    <div class="flex flex-col gap-1">
+      <label for="Email adress" class="text-black">Email</label>
+      <input type="text" name="email" id="" class="border-1 border-black m-4 text-black" />
+    </div>
+    <div class="flex flex-col gap-1">
+      <label for="Password" class="text-black">Password</label>
+      <input type="password" name="password" id="" class="border-1 border-black text-black" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
