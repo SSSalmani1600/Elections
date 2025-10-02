4d839247ffacfee856b61b496b1e1c40face7d5e
Niek Goedemans <goedemn@ad.hva.nl>
2025-10-02 23:37:43 +0200

    Add: Introsection on homepage

--- election-frontend/src/views/HomeView.vue
+++ election-frontend/src/views/HomeView.vue
@@ -3,7 +3,16 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="flex flex-col items-center">
+    <div class="flex flex-col items-center w-fit mt-[75px] gap-8">
+      <div class="flex flex-col items-center gap-3">
+      <h1>Maak kennis met de Nederlandse politiek</h1>
+      <p class="text-text-muted text-lg w-[60%] text-center"><strong>Leer</strong> partijen kennen, <strong>bekijk</strong> de uitslagen en <strong>ontdek</strong> jouw politieke match</p>
+      </div>
+      <div class="flex gap-4">
+        <button class="btn btn-primary">Ontdek verkiezingen</button>
+        <button class="btn btn-secondary">Doe de match quiz</button>
+      </div>
+    </div>
   </main>
 </template>
