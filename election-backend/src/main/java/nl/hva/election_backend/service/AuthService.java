d3547b586bbd45dc547ddfc7b7980451e7562319
Daan <daan.hoedjes@hva.nl>
2025-10-03 19:09:05 +0200

    add simple authenticate method to AuthService

--- election-backend/src/main/java/nl/hva/election_backend/service/AuthService.java
+++ election-backend/src/main/java/nl/hva/election_backend/service/AuthService.java
@@ -2,11 +2,23 @@ package nl.hva.election_backend.service;
 
 import nl.hva.election_backend.model.User;
 import nl.hva.election_backend.repo.InMemoryUserRepository;
+import nl.hva.election_backend.security.PasswordHasher;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class AuthService {
+    private final InMemoryUserRepository userRepo;
+    private final PasswordHasher hasher;
 
+    public AuthService(InMemoryUserRepository userRepo, PasswordHasher hasher) {
+        this.userRepo = userRepo;
+        this.hasher = hasher;
+    }
+
+    public boolean authenticate(String email, String password) {
+        User user = userRepo.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        return hasher.matches(password, user.getPasswordHash());
+    }
 }
