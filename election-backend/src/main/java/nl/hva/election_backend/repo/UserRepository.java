d8004c1c7cfafaf8d8b9c3c66f1def50a7960ac0
Daan <daan.hoedjes@hva.nl>
2025-10-03 19:05:29 +0200

    add User model and UserRepository

---
+++ election-backend/src/main/java/nl/hva/election_backend/repo/UserRepository.java
@@ -0,0 +1,12 @@
+package nl.hva.election_backend.repo;
+
+import nl.hva.election_backend.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository {
+    Optional<User> findByEmail(String email);
+    Optional<User> findById(Long id);
+    User save(User user);
+    boolean existsByEmail(String email);
+}
