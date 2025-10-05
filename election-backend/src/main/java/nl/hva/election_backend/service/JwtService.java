2e60fe190235fd5211e401e144d1ccf2cab2191b
Daan <daan.hoedjes@hva.nl>
2025-10-05 13:30:04 +0200

    replace JwtTokenProvider with JwtService and add extractAllClaims and isTokenExpired methods

---
+++ election-backend/src/main/java/nl/hva/election_backend/service/JwtService.java
@@ -0,0 +1,48 @@
+package nl.hva.election_backend.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+    private String SECRET_KEY = System.getenv().getOrDefault("JWT_SECRET_B64",
+            "dLRPokUNE7CfDTv2Nq1JmKZLuDSbMLvfTn9yJAxCx4A=");
+    private final String issuer = "ga-stemmen.nl";
+
+
+    public String generateToken(String displayName) {
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder().claims().add(claims).subject(displayName)
+                .issuer(issuer).issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 30))
+                .and().signWith(this.getKey()).compact();
+    }
+
+    private SecretKey getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean isTokenExpired(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.getExpiration().before(new Date());
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts
+            .parser()
+            .verifyWith(getKey())
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+    }
+}
