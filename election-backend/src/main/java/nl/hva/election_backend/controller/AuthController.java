4c098a0e5d0e1ffd3d6c2462ab750d0495985724
Daan <daan.hoedjes@hva.nl>
2025-10-05 15:00:59 +0200

    feat(auth): add POST /login endpoint to AuthController

--- election-backend/src/main/java/nl/hva/election_backend/controller/AuthController.java
+++ election-backend/src/main/java/nl/hva/election_backend/controller/AuthController.java
@@ -1,11 +1,14 @@
 package nl.hva.election_backend.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-
+    @PostMapping("/login")
+    public String login() {
+        return "";
+    }
 }
