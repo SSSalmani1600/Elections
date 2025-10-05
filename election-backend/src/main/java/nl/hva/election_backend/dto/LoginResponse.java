96a0fec998822b4b96ea7e6c25cf5ff5aff9b97d
Daan <daan.hoedjes@hva.nl>
2025-10-05 15:08:17 +0200

    feat(auth): add LoginRequest and LoginResponse DTOs

---
+++ election-backend/src/main/java/nl/hva/election_backend/dto/LoginResponse.java
@@ -0,0 +1,29 @@
+package nl.hva.election_backend.dto;
+
+public class LoginResponse {
+    private String displayName;
+    private String token;
+
+    public LoginResponse() {}
+
+    public LoginResponse(String displayName, String token) {
+        this.displayName = displayName;
+        this.token = token;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
