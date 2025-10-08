1b24d41f1c8b9a425f763df5785a041b062b0a02
Niek Goedemans <goedemn@ad.hva.nl>
2025-10-08 23:18:41 +0200

    Feat(overview-parties): Add - Affiliation interfaces

--- election-frontend/src/types/api.ts
+++ election-frontend/src/types/api.ts
@@ -2,3 +2,13 @@ export interface LoginResponse {
     displayName: string
     token: string
 }
+
+export interface Affiliation {
+  name: string;
+  candidates: string;
+}
+
+export interface AffiliationResponse {
+  id: string;
+  affiliations: Affiliation[];
+}
