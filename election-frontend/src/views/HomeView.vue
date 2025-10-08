e5c223c4a65c8976fb21c406904bcc93b6542342
Niek Goedemans <goedemn@ad.hva.nl>
2025-10-08 23:39:48 +0200

    Removed: h1 styling in main.css

--- election-frontend/src/views/HomeView.vue
+++ election-frontend/src/views/HomeView.vue
@@ -6,7 +6,7 @@ import TheWelcome from '../components/TheWelcome.vue'
   <main class="flex flex-col items-center">
     <div class="flex flex-col items-center w-fit mt-[75px] gap-8">
       <div class="flex flex-col items-center gap-3">
-      <h1>Maak kennis met de Nederlandse politiek</h1>
+      <h1 class="text-4xl font-bold">Maak kennis met de Nederlandse politiek</h1>
       <p class="text-text-muted text-lg w-[60%] text-center"><strong>Leer</strong> partijen kennen, <strong>bekijk</strong> de uitslagen en <strong>ontdek</strong> jouw politieke match</p>
       </div>
       <div class="flex gap-4">
