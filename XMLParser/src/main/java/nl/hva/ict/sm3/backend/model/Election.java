e2e8630cdf8a63ca5e7ca45117ef8598d5e5267d
Niek Goedemans <goedemn@ad.hva.nl>
2025-10-08 20:08:50 +0200

    Parser(Candidate): Add - Candidate transformer, Candidates stashed in Election

--- XMLParser/src/main/java/nl/hva/ict/sm3/backend/model/Election.java
+++ XMLParser/src/main/java/nl/hva/ict/sm3/backend/model/Election.java
@@ -1,14 +1,22 @@
 package nl.hva.ict.sm3.backend.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * This will hold the information for one specific election.<br/>
  * <b>This class is by no means production ready! You need to alter it extensively!</b>
  */
 public class Election {
     private final String id;
+    private Set<Candidate> candidates = new HashSet<>();
 
     public Election(String id) {
         this.id = id;
+    }
+
+    public Set<Candidate> getCandidates() {
+        return candidates;
     }
 
     @Override
--- XMLParser/src/main/java/nl/hva/ict/sm3/backend/utils/xml/transformers/DutchCandidateTransformer.java
+++ XMLParser/src/main/java/nl/hva/ict/sm3/backend/utils/xml/transformers/DutchCandidateTransformer.java
@@ -1,7 +1,9 @@
 package nl.hva.ict.sm3.backend.utils.xml.transformers;
 
+import nl.hva.ict.sm3.backend.model.Candidate;
 import nl.hva.ict.sm3.backend.model.Election;
 import nl.hva.ict.sm3.backend.utils.xml.CandidateTransformer;
+import nl.hva.ict.sm3.backend.utils.xml.TagAndAttributeNames;
 
 import java.util.Map;
 
@@ -9,7 +11,7 @@ import java.util.Map;
  * Just prints to content of electionData to the standard output.>br/>
  * <b>This class needs heavy modification!</b>
  */
-public class DutchCandidateTransformer implements CandidateTransformer {
+public class DutchCandidateTransformer implements CandidateTransformer, TagAndAttributeNames {
     private final Election election;
 
     /**
@@ -23,6 +25,15 @@ public class DutchCandidateTransformer implements CandidateTransformer {
 
     @Override
     public void registerCandidate(Map<String, String> electionData) {
-        System.out.println("Registering candidate: " + electionData);
+        String firstName = electionData.get(FIRST_NAME);
+        String lastName = electionData.get(LAST_NAME);
+        String initials = electionData.get(NAME_LINE);
+        String gender = electionData.get(GENDER);
+        String localityName = electionData.get(LOCALITY_NAME);
+        String electionId = electionData.get(ELECTION_IDENTIFIER);
+        String electionName = electionData.get(ELECTION_NAME);
+        String affiliationId = (electionData.get(AFFILIATION_IDENTIFIER));
+        Candidate candidate = new Candidate(firstName, lastName, initials, gender, localityName, electionId, electionName, affiliationId);
+        election.getCandidates().add(candidate);
     }
 }
