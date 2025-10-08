897d3fd997276123f6ac7c72ba22dadab94ce2f5
Daan <daan.hoedjes@hva.nl>
2025-10-08 21:31:33 +0200

    feat(auth): save JWT token in localStorage on succesful login

--- election-frontend/src/views/LoginView.vue
+++ election-frontend/src/views/LoginView.vue
@@ -1,73 +1,85 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue';
-import { useRouter } from 'vue-router';
+import { login } from '@/services/AuthService'
+import type { LoginResponse } from '@/types/api'
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
-const email = ref("");
-const password = ref("");
+const email = ref('')
+const password = ref('')
 
-const errors = reactive({
-    email: "",
-    password: "",
-});
+const error = reactive({
+    email: '',
+    password: '',
+})
 
 function isValidEmail(): void {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     if (!regex.test(email.value)) {
-        errors.email = "Voer een geldig email adres in.";
+        error.email = 'Voer een geldig email adres in.'
     } else if (!email.value) {
-        errors.email = "Email is verplicht."
+        error.email = 'Email is verplicht.'
     } else {
-        errors.email = "";
+        error.email = ''
     }
 }
 
-async function login(): Promise<void> {
-    isValidEmail();
-    errors.password = !password.value ? "Wachtwoord is verplicht." : "";
+async function loginHandler(): Promise<void> {
+    isValidEmail()
+    error.password = !password.value ? 'Wachtwoord is verplicht.' : ''
 
-    if ((errors.email || errors.password) !== "") return;
+    if ((error.email || error.password) !== '') return
 
-    const res = await fetch("http://localhost:8080/api/auth/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            email: email.value,
-            password: password.value
-        })
-    })
-
-    if (res.status === 200) {
-        router.resolve("/home");
+    try {
+        const data: LoginResponse = await login(email.value, password.value)
+        localStorage.setItem('JWT', data.token)
+        router.replace('/')
+    } catch (err: unknown) {
+        console.log(err)
+        error.email = 'Inloggen mislukt. Controleer je gegevens.'
     }
 }
 </script>
 
 <template>
-    <form @submit.prevent="login()" class="p-4 flex flex-col justify-around items-center p-24">
+    <form @submit.prevent="loginHandler()" class="flex flex-col justify-around items-center p-24">
         <h1 class="text-white text-3xl pb-8">Inloggen</h1>
         <div class="flex flex-col gap-8 w-[500px]">
             <div class="flex flex-col">
                 <label for="Email" class="sr-only">Email</label>
-                <input v-model="email" @input="errors.email = ''" type="text" name="email" id="" placeholder="Email"
-                    class="outline-[#191919] outline-[8px] rounded-t-[10px] border-[1px] border-[#262626]
-                    text-[20px] text-white bg-[#141414] py-2 px-4 placeholder-[#676767]" />
-                <span v-if="errors.email" class="text-red-500 text-sm mt-3">
-                    {{ errors.email }}</span>
+                <input
+                    v-model="email"
+                    @input="error.email = ''"
+                    type="text"
+                    name="email"
+                    id=""
+                    placeholder="Email"
+                    class="outline-[#191919] outline-[8px] rounded-t-[10px] border-[1px] border-[#262626] text-[20px] text-white bg-[#141414] py-2 px-4 placeholder-[#676767]"
+                />
+                <span v-if="error.email" class="text-red-500 text-sm mt-3"> {{ error.email }}</span>
             </div>
             <div class="flex flex-col">
                 <label for="Password" class="sr-only">Password</label>
-                <input v-model="password" @input="errors.password = ''" type="password" name="password" id=""
-                    placeholder="Wachtwoord" class="outline-[#191919] outline-[8px] rounded-t-[10px] border-[1px] border-[#262626]
-                    text-[20px] text-white bg-[#141414] py-2 px-4 placeholder-[#676767]" />
-                <span v-if="errors.password" class="text-red-500 text-sm mt-3">
-                    {{ errors.password }}</span>
+                <input
+                    v-model="password"
+                    @input="error.password = ''"
+                    type="password"
+                    name="password"
+                    id=""
+                    placeholder="Wachtwoord"
+                    class="outline-[#191919] outline-[8px] rounded-t-[10px] border-[1px] border-[#262626] text-[20px] text-white bg-[#141414] py-2 px-4 placeholder-[#676767]"
+                />
+                <span v-if="error.password" class="text-red-500 text-sm mt-3">
+                    {{ error.password }}</span
+                >
             </div>
-            <button type="submit" class="bg-[#EF3054] text-white p-3 text-[16px] rounded-[8px] cursor-pointer">Log
-                in</button>
+            <button
+                type="submit"
+                class="bg-[#EF3054] text-white p-3 text-[16px] rounded-[8px] cursor-pointer w-[90%] self-center"
+            >
+                Log in
+            </button>
         </div>
     </form>
 </template>
