6a728262635583b3271993096499f33031c4a0ad
Daan <daan.hoedjes@hva.nl>
2025-10-10 15:44:15 +0200

    update(auth): remove JWT token generation from /register endpoint

--- election-backend/src/main/java/nl/hva/election_backend/dto/RegisterResponse.java
+++ election-backend/src/main/java/nl/hva/election_backend/dto/RegisterResponse.java
@@ -3,4 +3,25 @@ package nl.hva.election_backend.dto;
 public class RegisterResponse {
     private String email;
     private String password;
+
+    public RegisterResponse(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
