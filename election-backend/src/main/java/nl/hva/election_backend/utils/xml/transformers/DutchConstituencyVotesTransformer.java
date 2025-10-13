c8d8376bd076804cf4133c89956b358dbb255e9d
Daan <daan.hoedjes@hva.nl>
2025-10-13 15:10:58 +0200

    feat(parser): implement registerPartyVotes to parse votes per party per constituency

--- election-backend/src/main/java/nl/hva/election_backend/utils/xml/transformers/DutchConstituencyVotesTransformer.java
+++ election-backend/src/main/java/nl/hva/election_backend/utils/xml/transformers/DutchConstituencyVotesTransformer.java
@@ -1,16 +1,21 @@
 package nl.hva.election_backend.utils.xml.transformers;
 
 
+import nl.hva.election_backend.model.Constituency;
 import nl.hva.election_backend.model.Election;
+import nl.hva.election_backend.model.Party;
+import nl.hva.election_backend.utils.xml.TagAndAttributeNames;
 import nl.hva.election_backend.utils.xml.VotesTransformer;
 
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Just prints to content of electionData to the standard output.>br/>
  * <b>This class needs heavy modification!</b>
  */
-public class DutchConstituencyVotesTransformer implements VotesTransformer {
+public class DutchConstituencyVotesTransformer implements VotesTransformer, TagAndAttributeNames {
     private final Election election;
 
     /**
@@ -24,12 +29,30 @@ public class DutchConstituencyVotesTransformer implements VotesTransformer {
 
     @Override
     public void registerPartyVotes(boolean aggregated, Map<String, String> electionData) {
-        System.out.printf("%s party votes: %s\n", aggregated ? "Constituency" : "Municipality", electionData);
+        String affiliationId = electionData.get(AFFILIATION_IDENTIFIER_ID);
+        String partyName = electionData.get(REGISTERED_NAME);
+        election.getParties().stream().filter(party -> Objects.equals(party.getName(), partyName))
+                .findFirst()
+                .ifPresent(party -> party.setId(affiliationId));
+
+        String constituencyName = electionData.get(CONTEST_NAME);
+        election.getConstituencies().add(new Constituency(constituencyName));
+        election.getConstituencies().forEach(constituency -> constituency.getParties().add(new Party(partyName)));
+        election.getConstituencies().forEach(constituency -> constituency.getParties()
+                .stream().filter(party -> Objects.equals(party.getName(), partyName))
+                .findFirst()
+                .ifPresent(party -> party.setId(affiliationId)));
+
+        int votes = Integer.parseInt(electionData.get(VALID_VOTES));
+        election.getConstituencies().forEach(constituency -> constituency.getParties()
+                .stream().filter(party -> Objects.equals(party.getId(), affiliationId))
+                .findFirst()
+                .ifPresent(party -> party.setVotes(party.getVotes() + votes)));
     }
 
     @Override
     public void registerCandidateVotes(boolean aggregated, Map<String, String> electionData) {
-        System.out.printf("%s candidate votes: %s\n", aggregated ? "Constituency" : "Municipality", electionData);
+
     }
 
     @Override
