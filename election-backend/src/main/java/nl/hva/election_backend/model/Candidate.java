e3a13c172f2e444859afc36eefe1caa8f06f6870
Manal El Hajoui <hajouim@hva.nl>
2025-10-16 09:18:04 +0200

    feat: make Candidate fields immutable and add election-related properties

--- election-backend/src/main/java/nl/hva/election_backend/model/Candidate.java
+++ election-backend/src/main/java/nl/hva/election_backend/model/Candidate.java
@@ -1,13 +1,15 @@
 package nl.hva.election_backend.model;
 
 public class Candidate {
-    private String firstName;
-    private String lastName;
-    private String initials;
-    private String gender;
-    private String localityName;
-    private String electionName;
-    private String partiesId;
+    private final String firstName;
+    private final String lastName;
+    private final String initials;
+    private final String gender;
+    private final String localityName;
+    private final String electionName;
+    private final String partiesId;
+    private String shortCode = null;
+    private boolean isElected = false;
 
     public Candidate(String firstName, String lastName, String initials, String gender, String localityName, String electionName, String affiliationId) {
         this.firstName = firstName;
@@ -17,6 +19,22 @@ public class Candidate {
         this.localityName = localityName;
         this.electionName = electionName;
         this.partiesId = affiliationId;
+    }
+
+    public boolean isElected() {
+        return isElected;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+
+    public void setElected(boolean elected) {
+        isElected = elected;
     }
 
     public String getFirstName() {
@@ -45,5 +63,16 @@ public class Candidate {
 
     public String getPartiesId() {
         return partiesId;
+    }
+
+    public String getNameLine() {
+        return null;
+    }
+
+    public void setVotes(int votes) {
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
     }
 }
