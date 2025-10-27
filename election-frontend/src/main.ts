33bebba2e45abaaf035e36fd6a367c6030f9e029
Daan <daan.hoedjes@hva.nl>
2025-10-27 15:07:43 +0100

    fix: default background color being overwritten by incorrect color

--- election-frontend/src/main.ts
+++ election-frontend/src/main.ts
@@ -3,16 +3,15 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/aura';
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
-
-const app = createApp(App);
+const app = createApp(App)
 app.use(PrimeVue, {
   theme: {
-    preset: Aura
-  }
-});
+    preset: Aura,
+  },
+})
 
 app.use(router)
 
