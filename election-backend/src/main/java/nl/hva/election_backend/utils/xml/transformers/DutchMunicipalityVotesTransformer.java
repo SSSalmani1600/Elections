94dd45ee7dd2b53b67e4c3e5b18c1ca808f90453
Nabil Salmani <nabil.salmani@hva.nl>
2025-10-29 16:06:55 +0100

    XML Parser voor DutchMunicipalityVotesTransformer

--- election-backend/src/main/java/nl/hva/election_backend/utils/xml/transformers/DutchMunicipalityVotesTransformer.java
+++ election-backend/src/main/java/nl/hva/election_backend/utils/xml/transformers/DutchMunicipalityVotesTransformer.java
@@ -1,7 +1,10 @@
 package nl.hva.election_backend.utils.xml.transformers;
 
 
+import nl.hva.election_backend.model.Candidate;
 import nl.hva.election_backend.model.Election;
+import nl.hva.election_backend.model.Party;
+import nl.hva.election_backend.utils.xml.TagAndAttributeNames;
 import nl.hva.election_backend.utils.xml.VotesTransformer;
 
 import java.util.Map;
@@ -23,8 +26,27 @@ public class DutchMunicipalityVotesTransformer implements VotesTransformer {
     }
 
     @Override
-    public void registerPartyVotes(boolean aggregated, Map<String, String> electionData) {
-        System.out.printf("%s party votes: %s\n", aggregated ? "Municipality" : "Polling station", electionData);
+    public void registerPartyVotes(boolean aggregated, Map<String, String> data) {
+        String partyId = data.get(TagAndAttributeNames.AFFILIATION_IDENTIFIER_ID);
+        String votesStr = data.get(TagAndAttributeNames.VALID_VOTES);
+        if (partyId == null || votesStr == null) return;
+
+        int votes = safeInt(votesStr);
+
+        String municipalityId = data.get(TagAndAttributeNames.SUPERIOR_REGION_NUMBER);
+        String municipalityName = data.get(TagAndAttributeNames.REGION_NAME);
+        String stationId = data.get(TagAndAttributeNames.REPORTING_UNIT_IDENTIFIER_ID);
+
+        Party party = election.findPartyById(partyId);
+        if (party == null) return;
+
+        if (aggregated) {
+            String key = safeKey(municipalityId, municipalityName);
+            party.getVotesByMunicipality().merge(key, votes, Integer::sum);
+        } else {
+            String key = safeKey(stationId, municipalityName);
+            party.getVotesByStation().merge(key, votes, Integer::sum);
+        }
     }
 
     @Override
