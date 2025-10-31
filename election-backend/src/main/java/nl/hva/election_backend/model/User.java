14b9ad41f8e5be6702b8b067071b660944040436
Niek Goedemans <goedemn@ad.hva.nl>
2025-10-31 14:04:52 +0100

    created back-end endpoint, added to whitelist, created test repository for user

--- election-backend/src/main/java/nl/hva/election_backend/model/User.java
+++ election-backend/src/main/java/nl/hva/election_backend/model/User.java
@@ -1,6 +1,13 @@
 package nl.hva.election_backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
     private Long id;
     private String email;
     private String passwordHash;
