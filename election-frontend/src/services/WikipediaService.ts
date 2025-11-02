daa1cc74d33d487440534d13504967a78e845d7e
Afnaan Makhloufi <afnaan.makhloufi@hva.nl>
2025-11-02 18:27:40 +0100

    feat(wiki): tolerant summary fetch with search fallback

---
+++ election-frontend/src/services/WikipediaService.ts
@@ -0,0 +1,27 @@
+export async function getWikipediaSummary(query: string) {
+  let response = await fetch(
+    `https://nl.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(query)}`,
+  )
+  if (response.status === 404) {
+    const searchUrl = `https://nl.wikipedia.org/w/api.php?action=query&list=search&srsearch=${encodeURIComponent(
+      query,
+    )}&format=json&origin=*`
+    const searchRes = await fetch(searchUrl)
+    const searchData = await searchRes.json()
+    const firstTitle = searchData.query?.search?.[0]?.title
+
+    if (firstTitle){
+      console.log(`Geen exacte match, probeer met titel: ${firstTitle}`)
+      response = await fetch(
+          `https://nl.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(firstTitle)}`
+        )
+      }
+    const data = await response.json()
+    console.log('Wikipedia data:', data)
+
+    return {
+      title: data.title,
+      summary: data.extract
+    }
+  }
+}
