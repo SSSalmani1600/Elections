14287e5d193e25818ef6440ff7b730cb71838aff
Nabil Salmani <nabil.salmani@hva.nl>
2025-11-02 15:07:29 +0100

    import updated

--- election-backend/src/main/java/nl/hva/election_backend/dto/DiscussionListItemDto.java
+++ election-backend/src/main/java/nl/hva/election_backend/dto/DiscussionListItemDto.java
@@ -2,7 +2,7 @@ package nl.hva.election_backend.dto;
 
 
 import java.time.Instant;
-
+s
 // dit is data van 1 discussie, wordt naar frontend gestuurd
 public record DiscussionListItemDto(
         String id,
