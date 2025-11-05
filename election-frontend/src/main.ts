778a398edcc914bc217d640ec2c374e06087463a
Manal El Hajoui <hajouim@hva.nl>
2025-11-05 22:42:22 +0100

    Initialize Vue 3 app with PrimeVue and router configuration

--- election-frontend/src/main.ts
+++ election-frontend/src/main.ts
@@ -1,3 +1,4 @@
+// main.ts
 import './assets/main.css'
 
 import { createApp } from 'vue'
@@ -5,14 +6,23 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
+import { createPinia } from 'pinia'
+import { useAuthStore } from '@/store/useAuthStore'  // ✅ importeer je store
 
 const app = createApp(App)
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-  },
-})
+const pinia = createPinia()
 
+app.use(PrimeVue, { theme: { preset: Aura } })
+app.use(pinia)
 app.use(router)
+
+
+const auth = useAuthStore()
+const savedToken = localStorage.getItem('JWT')
+const savedUsername = localStorage.getItem('username')
+
+if (savedToken && savedUsername) {
+  auth.login(savedUsername, savedToken)
+}
 
 app.mount('#app')
