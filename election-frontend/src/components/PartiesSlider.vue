f6768ffd932f78ae93dddd5f5cfc8614fe9c9b1b
Niek Goedemans <goedemn@ad.hva.nl>
2025-11-06 20:10:51 +0100

    fix(slider-layout): add - navigation arrows with styling

--- election-frontend/src/components/PartiesSlider.vue
+++ election-frontend/src/components/PartiesSlider.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import {onMounted, onUnmounted, ref} from 'vue';
+import {nextTick, onMounted, onUnmounted, ref} from 'vue';
 import Swiper from 'swiper';
 
 // Import Swiper styles
@@ -27,6 +27,9 @@ onMounted(async () => {
   } finally {
     loading.value = false;
   }
+
+  await nextTick();
+
   new Swiper('.swiper', {
     modules: [Grid, Pagination, Navigation, Autoplay],
     slidesPerView: 1,
@@ -36,10 +39,10 @@ onMounted(async () => {
     },
 
     speed: 1000,
-    autoplay: {
-      pauseOnMouseEnter: true,
-      delay: 4000,
-    },
+    // autoplay: {
+    //   pauseOnMouseEnter: true,
+    //   delay: 4000,
+    // },
     spaceBetween: 20,
     slidesPerGroup: 1,
     pagination: {
@@ -49,8 +52,12 @@ onMounted(async () => {
       dynamicBullets: true,
       dynamicMainBullets: 3,
     },
+    navigation: {
+      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next'
+    },
     breakpoints: {
-      1480: {
+      1920: {
         slidesPerView: 4,
         slidesPerGroup: 4,
         grid: {
@@ -87,8 +94,8 @@ onMounted(async () => {
         }
       },
       768: {
-        slidesPerView: 3,
-        slidesPerGroup: 3,
+        slidesPerView: 2,
+        slidesPerGroup: 2,
         grid: {
           rows: 2,
           fill: 'row',
@@ -99,8 +106,8 @@ onMounted(async () => {
         }
       },
       440: {
-        slidesPerView: 2,
-        slidesPerGroup: 2,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
         grid: {
           rows: 2,
           fill: 'row',
@@ -117,7 +124,7 @@ onMounted(async () => {
 
 <template>
   <div class="flex gap-16 w-[80%] items-center justify-center max-lg:flex-col">
-    <div class="w-[60%] flex flex-col relative min-h-[200px] gap-4 max-lg:order-1 max-lg:w-full">
+    <div class="w-[60%] flex flex-col relative min-h-[164px] gap-4 max-lg:order-1 max-lg:w-full">
       <div class="flex justify-between items-center">
         <span class="text-2xl font-bold">Partijen</span>
         <router-link to="/partijen" class="text-primary link">Bekijk alles</router-link>
@@ -126,20 +133,28 @@ onMounted(async () => {
             class="text-lg bg-background py-4 px-8 rounded-lg shadow-lg text-primary absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-full md:w-fit">{{
           error
         }}</span>
-      <div class="swiper">
-        <div class="swiper-wrapper overflow-y-auto">
-          <router-link :to="{path: '/partij/' + party}" v-for="(party, index) in data" :key="party"
-                       class="swiper-slide p-4 px-10 h-fit w-full flex items-center justify-between bg-background text-white rounded-xl z-20">
-            <div class="flex items-center gap-4 overflow-hidden w-full">
-              <div class="w-[60px] h-[60px] shrink-0">
-                <img v-if="index % 2 == 0" src="../assets/fvd-logo.png" class="w-full h-full object-contain" alt="">
-                <img v-if="index % 2 !== 0" src="../assets/d66-logo.png"
-                     class="w-full h-full object-contain" alt="">
+      <div class="relative">
+        <div class="swiper">
+          <div class="swiper-wrapper overflow-y-auto">
+            <router-link :to="{path: '/partij/' + party}" v-for="(party, index) in data"
+                         :key="party"
+                         class="swiper-slide p-4 px-10 h-fit w-full flex items-center justify-between bg-background text-white rounded-xl z-20">
+              <div class="flex items-center gap-4 overflow-hidden w-full">
+                <div class="w-[40px] h-[40px] shrink-0">
+                  <img v-if="index % 2 == 0" src="../assets/fvd-logo.png"
+                       class="w-full h-full object-contain" alt="">
+                  <img v-if="index % 2 !== 0" src="../assets/d66-logo.png"
+                       class="w-full h-full object-contain" alt="">
+                </div>
+                <span class="font-semibold truncate text-white">{{ party }}</span>
               </div>
-              <span class="font-semibold truncate text-white">{{ party }}</span>
-            </div>
-          </router-link>
+            </router-link>
+          </div>
+
         </div>
+        <div class="swiper-button-prev navigation-btn"><i class="pi pi-angle-left"></i></div>
+        <div class="swiper-button-next navigation-btn"><i class="pi pi-angle-right"></i></div>
+
         <div class="swiper-pagination"></div>
       </div>
     </div>
@@ -150,26 +165,58 @@ onMounted(async () => {
 <style scoped>
 .swiper {
   width: 100%;
-  padding-bottom: 2rem;
 }
 
-:deep(.swiper-pagination) {
+.swiper-pagination {
   display: flex;
   gap: 4px;
+  position: absolute;
+  bottom: -25px;
+  left: 50%;
+  transform: translateX(-50%);
+  justify-content: center;
+
+}
+
+:deep(.navigation-btn svg) {
+  display: none !important;
+}
+
+.navigation-btn {
+  position: absolute;
+  color: white;
+  cursor: pointer;
+  transform: translateY(-50%);
+  bottom: 50%;
+  width: 28px;
+  height: 28px;
+  margin-top: 0;
+}
+
+.navigation-btn i {
+  font-size: 24px !important;
+}
+
+.swiper-button-next {
+  right: -40px;
+}
+
+.swiper-button-prev {
+  left: -40px;
 }
 
 :deep(.swiper-pagination-bullet) {
   background: white;
   opacity: 0.5;
   transition-duration: 1000ms;
-  padding: 3px;
+  min-width: 7px;
 }
 
 :deep(.swiper-pagination-bullet-active) {
   opacity: 1;
 }
 
-:deep(.swiper-slide) {
+.swiper-slide {
   display: flex;
   align-items: center;
   justify-content: center;
