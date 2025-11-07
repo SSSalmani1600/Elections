e498511b96c82debd886f3a58c2c50a714e4073f
Niek Goedemans <goedemn@ad.hva.nl>
2025-11-07 15:08:41 +0100

    fix(dynamic-party-images): add - wikipedia request for description and image

--- election-frontend/src/services/WikipediaService.ts
+++ election-frontend/src/services/WikipediaService.ts
@@ -10,7 +10,7 @@ export async function getWikipediaSummary(query: string) {
     const searchData = await searchRes.json()
     const firstTitle = searchData.query?.search?.[0]?.title
 
-    if (firstTitle){
+    if (firstTitle) {
       console.log(`Geen exacte match, probeer met titel: ${firstTitle}`)
       response = await fetch(
         `https://nl.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(firstTitle)}`
@@ -27,15 +27,69 @@ export async function getWikipediaSummary(query: string) {
     image: data.thumbnail?.source,
   }
 }
+
 export async function getWikipediaPerson(name: string) {
   const url = `https://nl.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(name)}`
   const res = await fetch(url)
 
 
   const data = await res.json()
+  const firstTitle = data.query?.search?.[0]?.title
+
+  if (firstTitle) {
+
+  }
   return {
     title: data.title,
     description: data.description,
     image: data.thumbnail?.source || '',
   }
 }
+
+export async function getWikipediaPartyData(query: string) {
+  const endpoint = "https://nl.wikipedia.org/w/api.php";
+
+  const searchParams = new URLSearchParams({
+    action: "query",
+    list: "search",
+    srsearch: query,
+    format: "json",
+    origin: "*",
+  });
+
+  const searchResponse = await fetch(`${endpoint}?${searchParams}`);
+  const searchData = await searchResponse.json();
+
+  if (!searchData.query?.search?.length) {
+    console.warn("Geen resultaten gevonden");
+    return null;
+  }
+
+  const pageTitle = searchData.query.search[0].title;
+
+  const infoParams = new URLSearchParams({
+    action: "query",
+    titles: pageTitle,
+    prop: "pageimages|extracts",
+    piprop: "thumbnail",
+    pithumbsize: "500",
+    exintro: "true",
+    explaintext: "true",
+    format: "json",
+    origin: "*",
+  });
+
+  const infoResponse = await fetch(`${endpoint}?${infoParams}`);
+  const infoData = await infoResponse.json();
+
+  const pages = infoData.query?.pages;
+  const firstPage = pages ? Object.values(pages)[0] as any : null;
+
+  console.log(firstPage)
+  return {
+    title: pageTitle,
+    summary: firstPage?.extract || "Geen samenvatting gevonden.",
+    img: firstPage?.thumbnail?.source || "",
+  };
+}
+
