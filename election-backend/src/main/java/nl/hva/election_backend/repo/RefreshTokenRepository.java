903994ee425995cbd41bde689a7de0e291dbc5ca
Daan <daan.hoedjes@hva.nl>
2025-11-21 12:49:43 +0100

    add: add method to validate refreshToken in RefreshTokenRepository

--- election-backend/src/main/java/nl/hva/election_backend/repo/RefreshTokenRepository.java
+++ election-backend/src/main/java/nl/hva/election_backend/repo/RefreshTokenRepository.java
@@ -4,6 +4,13 @@ import nl.hva.election_backend.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+    Optional<RefreshToken> findByTokenHash(String tokenHash);
+
+    boolean existsByTokenHashAndRevokedAtIsNullAndExpiresAtAfter(String tokenHash, Instant now);
 }
