6369b82d32f5bedd822ab4a6571a25b1abf344d8
Nabil Salmani <nabil.salmani@hva.nl>
2025-11-24 23:52:59 +0100

    kan bijna account bewerken.

---
+++ election-backend/src/main/java/nl/hva/election_backend/dto/UpdateUserRequest.java
@@ -0,0 +1,34 @@
+package nl.hva.election_backend.dto;
+
+public class UpdateUserRequest {
+    private String username;
+    private String email;
+    private String password; // Optioneel - alleen als wachtwoord moet worden gewijzigd
+
+    public UpdateUserRequest() {}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
