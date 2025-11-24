077214a61f6d2f6d75a72dbc623902cfee34d645
Nabil Salmani <nabil.salmani@hva.nl>
2025-11-24 21:39:12 +0100

    revert inlog en register

--- election-backend/src/main/java/nl/hva/election_backend/controller/AuthController.java
+++ election-backend/src/main/java/nl/hva/election_backend/controller/AuthController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:3000"})
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174", "http://localhost:3000"})
 public class AuthController {
     private final AuthService authService;
     private final JwtService jwtService;
