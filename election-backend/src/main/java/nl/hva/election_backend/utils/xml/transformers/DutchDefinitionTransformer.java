99208a95f149f78eb5433906aa3361b3785dd82a
Daan <daan.hoedjes@hva.nl>
2025-11-24 15:08:21 +0100

    add: logic for storing municipalities in database to DutchDefinitionTransformer

--- election-backend/src/main/java/nl/hva/election_backend/utils/xml/transformers/DutchDefinitionTransformer.java
+++ election-backend/src/main/java/nl/hva/election_backend/utils/xml/transformers/DutchDefinitionTransformer.java
@@ -1,13 +1,18 @@
 package nl.hva.election_backend.utils.xml.transformers;
 
 
+import io.micrometer.common.util.StringUtils;
+import nl.hva.election_backend.entity.MunicipalityEntity;
 import nl.hva.election_backend.entity.PartyEntity;
 import nl.hva.election_backend.model.Party;
 import nl.hva.election_backend.model.Election;
+import nl.hva.election_backend.service.MunicipalityService;
 import nl.hva.election_backend.service.PartyService;
 import nl.hva.election_backend.utils.xml.DefinitionTransformer;
+import nl.hva.election_backend.utils.xml.TagAndAttributeNames;
 
 import java.util.Map;
+import java.util.Objects;
 
 import static nl.hva.election_backend.utils.xml.TagAndAttributeNames.*;
 
@@ -15,9 +20,10 @@ import static nl.hva.election_backend.utils.xml.TagAndAttributeNames.*;
  * Just prints to content of electionData to the standard output.>br/>
  * <b>This class needs heavy modification!</b>
  */
-public class DutchDefinitionTransformer implements DefinitionTransformer {
+public class DutchDefinitionTransformer implements DefinitionTransformer, TagAndAttributeNames {
     private final Election election;
     private final PartyService partyService;
+    private final MunicipalityService municipalityService;
     private int currentPartyId = 1;
 
     /**
@@ -25,9 +31,10 @@ public class DutchDefinitionTransformer implements DefinitionTransformer {
      * It expects an instance of Election that can be used for storing the results.
      * @param election the election in which the votes wil be stored.
      */
-    public DutchDefinitionTransformer(Election election, PartyService partyService) {
+    public DutchDefinitionTransformer(Election election, PartyService partyService, MunicipalityService municipalityService) {
         this.election = election;
         this.partyService = partyService;
+        this.municipalityService = municipalityService;
     }
 
     @Override
@@ -38,7 +45,31 @@ public class DutchDefinitionTransformer implements DefinitionTransformer {
             System.out.println("Election metadata loaded: " + election);
         }
 
-        System.out.println("Committee/Region: " + electionData);
+        String category = electionData.get(REGION_CATEGORY_ATTRIBUTE);
+
+        if (!Objects.equals(category, "GEMEENTE")) return;
+
+        String year = electionData.get(ELECTION_DATE).split("-")[0];
+        String contestId = electionData.get(SUPERIOR_REGION_NUMBER_ATTRIBUTE);
+        String municipalityId = electionData.get(REGION_NUMBER_ATTRIBUTE);
+        String name = electionData.get(REGION_NAME);
+
+        MunicipalityEntity savedMunicipality = null;
+
+        if (StringUtils.isNotBlank(year) &&
+                StringUtils.isNotBlank(municipalityId) &&
+                StringUtils.isNotBlank(contestId) &&
+                StringUtils.isNotBlank(name)) {
+            savedMunicipality = municipalityService
+                    .saveIfNotExists(Integer.parseInt(year), municipalityId, contestId, name);
+        }
+
+        if (savedMunicipality == null) {
+            System.out.println("municipality already exists in db");
+            return;
+        }
+
+        System.out.println("Municipality saved: " + savedMunicipality.getYear() + " - " + savedMunicipality.getMunicipalityId());
     }
     @Override
     public void registerParty(Map<String, String> electionData) {
