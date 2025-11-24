81d594ed11ea057a0b6be06fcaa90d166135272d
Daan <daan.hoedjes@hva.nl>
2025-11-24 11:11:52 +0100

    add: saveResultIfNotExists method in ConstituencyService

--- election-backend/src/main/java/nl/hva/election_backend/service/ConstituencyService.java
+++ election-backend/src/main/java/nl/hva/election_backend/service/ConstituencyService.java
@@ -1,15 +1,19 @@
 package nl.hva.election_backend.service;
 
 import nl.hva.election_backend.entity.ConstituencyEntity;
+import nl.hva.election_backend.entity.ConstituencyResultEntity;
 import nl.hva.election_backend.repository.ConstituencyRepository;
+import nl.hva.election_backend.repository.ConstituencyResultRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConstituencyService {
     private final ConstituencyRepository constituencyRepository;
+    private final ConstituencyResultRepository resultRepository;
 
-    public ConstituencyService(ConstituencyRepository constituencyRepository) {
+    public ConstituencyService(ConstituencyRepository constituencyRepository, ConstituencyResultRepository resultRepository) {
         this.constituencyRepository = constituencyRepository;
+        this.resultRepository = resultRepository;
     }
 
     public ConstituencyEntity saveIfNotExists(String id, int year, String name) {
@@ -18,5 +22,13 @@ public class ConstituencyService {
         }
 
         return constituencyRepository.save(new ConstituencyEntity(id, year, name));
+    }
+
+    public ConstituencyResultEntity saveResultIfNotExists(int year, String constituencyId, String partyId, int validVotes) {
+        if (resultRepository.existsByYearAndConstituencyIdAndPartyId(year, constituencyId, partyId)) {
+            return null;
+        }
+
+        return resultRepository.save(new ConstituencyResultEntity(year, constituencyId, partyId, validVotes));
     }
 }
