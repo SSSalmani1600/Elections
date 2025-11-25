6aaea19dda9009b8b2c754a361773e24e37b8ff1
Niek Goedemans <goedemn@ad.hva.nl>
2025-11-25 18:47:04 +0100

    feat(all-statements-from-db): add - functionality for selecting question, standard selects first unanswered question

--- election-frontend/src/views/VotingGuideView.vue
+++ election-frontend/src/views/VotingGuideView.vue
@@ -6,12 +6,26 @@ import {getAllStatements} from "@/services/StatementService.ts";
 
 const data = ref<Statement[]>([]);
 const loading = ref<boolean>(false);
+const selectedStatement = ref<Statement>();
 
+const selectStatement = (statement: Statement) => {
+  selectedStatement.value = statement;
+}
 onMounted(async () => {
   try {
     loading.value = true;
     data.value = Array.from(await getAllStatements())
     console.log(data.value)
+
+    const firstUnanswered = data.value.find(statement => {
+      return !localStorage.getItem(`answer_${statement.id}`);
+    });
+
+    if (firstUnanswered) {
+      selectedStatement.value = firstUnanswered;
+    } else if (data.value.length > 0) {
+      selectedStatement.value = data.value[0];
+    }
   } catch (err: any) {
     console.error(err.message);
   } finally {
@@ -31,12 +45,13 @@ onMounted(async () => {
           <span class="skeleton-text h-4 w-[40%] rounded-[10px]"></span>
         </button>
 
-        <button v-if="!loading" v-for="(statement, index) in data" class="flex flex-col cursor-pointer gap-1 p-4 items-start hover:bg-primary duration-300">
+        <button v-if="!loading" v-for="(statement, index) in data" :key="statement.id" @click="selectStatement(statement)" :class="selectedStatement?.id === statement.id ? 'bg-primary' : ''" class="flex flex-col cursor-pointer gap-1 p-4 items-start hover:bg-primary duration-300">
           <span class="font-bold text-2xl truncate">{{ index + 1 }} - {{ statement.statement }}</span>
           <span class="opacity-80">{{ statement.category }}</span>
         </button>
       </div>
-      <div></div>
+      <div>
+      </div>
     </div>
   </div>
 </template>
