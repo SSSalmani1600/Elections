781ec739f0244786afb831588f41b8596176e776
Niek Goedemans <goedemn@ad.hva.nl>
2025-11-25 20:13:00 +0100

    feat(all-statements-from-db): add - html and styling for opinion buttons

--- election-frontend/src/views/VotingGuideView.vue
+++ election-frontend/src/views/VotingGuideView.vue
@@ -6,7 +6,7 @@ import {getAllStatements} from "@/services/StatementService.ts";
 
 const data = ref<Statement[]>([]);
 const loading = ref<boolean>(false);
-const selectedStatement = ref<Statement>();
+const selectedStatement = ref<Statement | null>(null);
 
 const selectStatement = (statement: Statement) => {
   selectedStatement.value = statement;
@@ -36,8 +36,8 @@ onMounted(async () => {
 
 <template>
   <div class="flex flex-col gap-4 m-[54px]">
-    <span class="font-bold text-[28px]">VRAGEN</span>
-    <div class="grid grid-cols-12">
+    <span class="font-bold text-[28px]">STELINGEN</span>
+    <div class="grid grid-cols-12 gap-20">
 
       <div class="col-span-3 bg-background rounded-[10px] max-h-[575px] overflow-x-hidden">
         <button v-if="loading" v-for="i in 10" class="flex flex-col gap-2.5 p-4 items-start w-full">
@@ -45,18 +45,53 @@ onMounted(async () => {
           <span class="skeleton-text h-4 w-[40%] rounded-[10px]"></span>
         </button>
 
-        <button v-if="!loading" v-for="(statement, index) in data" :key="statement.id" @click="selectStatement(statement)" :class="selectedStatement?.id === statement.id ? 'bg-primary' : ''" class="flex flex-col cursor-pointer gap-1 p-4 items-start hover:bg-primary duration-300">
-          <span class="font-bold text-2xl truncate">{{ index + 1 }} - {{ statement.statement }}</span>
+        <button v-if="!loading" v-for="(statement, index) in data" :key="statement.id"
+                @click="selectStatement(statement)"
+                :class="selectedStatement?.id === statement.id ? 'bg-primary' : ''"
+                class="flex flex-col cursor-pointer gap-1 p-4 items-start hover:bg-primary duration-300">
+          <span class="font-bold text-2xl truncate">{{ index + 1 }} - {{
+              statement.statement
+            }}</span>
           <span class="opacity-80">{{ statement.category }}</span>
         </button>
       </div>
-      <div>
+      <div class="col-span-7 flex flex-col gap-10">
+        <div class="flex flex-col gap-6">
+          <div class="w-full flex justify-between items-center pb-6 border-b-2 border-white">
+            <span class="px-2.5 py-2 rounded-[50px] bg-primary">{{
+                selectedStatement?.category
+              }}</span>
+            <span class="font-bold text-3xl">STELLING - {{ selectedStatement?.id }}</span>
+          </div>
+          <div class="flex flex-col gap-4 bg-background p-4 rounded-lg">
+            <p class="text-[28px] font-bold">{{ selectedStatement?.statement }}</p>
+            <p class="opacity-80 text-lg">{{ selectedStatement?.explanation }}</p>
+          </div>
+        </div>
+        <div class="flex flex-col gap-4">
+          <span class="font-bold text-[28px]">Hiermee ben ik het</span>
+          <div class="flex items-center gap-3 text-lg">
+            <button class="btn opinion-btn !border-[#48D507] hover:bg-[#48D507]">EENS</button>
+            <span class="text-2xl">/</span>
+            <button class="btn opinion-btn hover:bg-white hover:text-black">NEUTRAAL</button>
+            <span class="text-2xl">/</span>
+            <button class="btn opinion-btn !border-[#FF1E00] hover:bg-[#FF1E00]">ONEENS</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.opinion-btn {
+  padding: 4px 10px !important;
+  font-weight: 600;
+  border: 1px solid;
+  transition-duration: 400ms;
+  border-radius: 10px;
+}
+
 .skeleton-text {
   background: linear-gradient(90deg, #3e3e3e 0%, #555 50%, #3e3e3e 100%);
   background-size: 200% 100%;
