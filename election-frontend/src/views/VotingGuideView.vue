34f744333cd885cfea49124f470c230b0ac9fc0e
Niek Goedemans <goedemn@ad.hva.nl>
2025-11-25 17:23:01 +0100

    feat(all-statements-from-db): add - retrieving data from back end to frontend

--- election-frontend/src/views/VotingGuideView.vue
+++ election-frontend/src/views/VotingGuideView.vue
@@ -1,8 +1,35 @@
 <script setup lang="ts">
+
+import {onMounted, ref} from "vue";
+import type {Statement} from "@/types/api.ts";
+import {getAllStatements} from "@/services/StatementService.ts";
+
+const data = ref<Statement[]>([]);
+const loading = ref<boolean>(false);
+
+onMounted(async () => {
+  try {
+    loading.value = true;
+    data.value = Array.from(await getAllStatements())
+    console.log(data.value)
+  } catch (err: any) {
+    console.error(err.message);
+  } finally {
+    loading.value = false;
+  }
+})
 </script>
 
 <template>
-Dit is de stemwijzer
+  <div>
+    <span>VRAGEN</span>
+    <div>
+      <div v-for="(statement, index) in data">
+        <span>{{ index + 1 }} - {{ statement.statement }}</span>
+        <span>{{ statement.category}}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
