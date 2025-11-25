b74c5b9417e19b4a16e97fc61ced01fa6e18fe0f
Niek Goedemans <goedemn@ad.hva.nl>
2025-11-25 18:25:34 +0100

    feat(all-statements-from-db): add - styling for the list of statements, skeleton frame while loading

--- election-frontend/src/views/VotingGuideView.vue
+++ election-frontend/src/views/VotingGuideView.vue
@@ -21,17 +21,39 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div>
-    <span>VRAGEN</span>
-    <div>
-      <div v-for="(statement, index) in data">
-        <span>{{ index + 1 }} - {{ statement.statement }}</span>
-        <span>{{ statement.category}}</span>
+  <div class="flex flex-col gap-4 m-[54px]">
+    <span class="font-bold text-[28px]">VRAGEN</span>
+    <div class="grid grid-cols-12">
+
+      <div class="col-span-3 bg-background rounded-[10px] max-h-[575px] overflow-x-hidden">
+        <button v-if="loading" v-for="i in 10" class="flex flex-col gap-2.5 p-4 items-start w-full">
+          <span class="skeleton-text h-4 w-full rounded-[10px]"></span>
+          <span class="skeleton-text h-4 w-[40%] rounded-[10px]"></span>
+        </button>
+
+        <button v-if="!loading" v-for="(statement, index) in data" class="flex flex-col cursor-pointer gap-1 p-4 items-start hover:bg-primary duration-300">
+          <span class="font-bold text-2xl truncate">{{ index + 1 }} - {{ statement.statement }}</span>
+          <span class="opacity-80">{{ statement.category }}</span>
+        </button>
       </div>
+      <div></div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.skeleton-text {
+  background: linear-gradient(90deg, #3e3e3e 0%, #555 50%, #3e3e3e 100%);
+  background-size: 200% 100%;
+  animation: skeleton-loading 1.2s infinite;
+}
 
+@keyframes skeleton-loading {
+  0% {
+    background-position: 200% 0;
+  }
+  100% {
+    background-position: -200% 0;
+  }
+}
 </style>
