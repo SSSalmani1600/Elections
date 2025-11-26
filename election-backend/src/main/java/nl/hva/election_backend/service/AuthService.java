489a8ed4082b1cbebc2b4ddcbc654aaa721286ea
Daan <daan.hoedjes@hva.nl>
2025-11-26 14:51:19 +0100

    add: refreshTokens method to both rotate refresh token and generate new access token

--- election-backend/src/main/java/nl/hva/election_backend/service/AuthService.java
+++ election-backend/src/main/java/nl/hva/election_backend/service/AuthService.java
@@ -1,6 +1,7 @@
 package nl.hva.election_backend.service;
 
 import nl.hva.election_backend.dto.AuthenticationResponse;
+import nl.hva.election_backend.dto.TokenRefreshResponse;
 import nl.hva.election_backend.model.RefreshToken;
 import nl.hva.election_backend.model.User;
 import nl.hva.election_backend.repository.RefreshTokenRepository;
@@ -11,13 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.SecureRandom;
 import java.time.Instant;
-import java.util.Base64;
-import java.util.Date;
 
 @Transactional
 @Service
@@ -27,8 +22,6 @@ public class AuthService {
     private final BCryptPasswordHasher hasher;
     private final JwtService jwtService;
     private final RefreshTokenRepository refreshRepo;
-    private final String SECRET_KEY = System.getenv().getOrDefault("JWT_SECRET_B64",
-            "dLRPokUNE7CfDTv2Nq1JmKZLuDSbMLvfTn9yJAxCx4A=");
 
     public AuthService(UserRepository userRepo, BCryptPasswordHasher hasher, JwtService jwtService, RefreshTokenRepository refreshRepo) {
         this.userRepo = userRepo;
@@ -36,7 +29,6 @@ public class AuthService {
         this.jwtService = jwtService;
         this.refreshRepo = refreshRepo;
     }
-
 
     @Transactional
     public AuthenticationResponse authenticate(String email, String password) {
@@ -48,9 +40,10 @@ public class AuthService {
 
         String accessToken = "";
         String refreshTokenHash = "";
+
         try {
             refreshTokenHash = jwtService.generateRefreshToken();
-            accessToken = jwtService.generateToken(user.getId().toString());
+            accessToken = jwtService.generateToken(user.getId());
         } catch (Exception e) {
             log.error("e: ", e);
         }
@@ -60,6 +53,13 @@ public class AuthService {
         refreshRepo.saveAndFlush(refreshToken);
 
         return new AuthenticationResponse(accessToken, refreshTokenHash, user);
+    }
+    public TokenRefreshResponse refreshTokens(String refreshTokenHash) {
+        RefreshToken newRefreshToken = jwtService.rotateRefreshToken(refreshTokenHash);
+
+        String newAccessToken = jwtService.generateToken(newRefreshToken.getUserId());
+
+        return new TokenRefreshResponse(newRefreshToken.getTokenHash(), newAccessToken);
     }
 
     public User register(String email, String rawPassword, String username) {
