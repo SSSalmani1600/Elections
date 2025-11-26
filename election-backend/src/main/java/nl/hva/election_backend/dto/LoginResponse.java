92d9e10b0b2dd41f3267850fd1eec7b3b4eb9052
Daan <daan.hoedjes@hva.nl>
2025-11-26 20:51:04 +0100

    rework: change LoginResponse to not allocate a User object unnecessarily

--- election-backend/src/main/java/nl/hva/election_backend/dto/LoginResponse.java
+++ election-backend/src/main/java/nl/hva/election_backend/dto/LoginResponse.java
@@ -3,20 +3,50 @@ package nl.hva.election_backend.dto;
 import nl.hva.election_backend.model.User;
 
 public class LoginResponse {
-    private User user;
+    private Long id;
+    private String email;
+    private String username;
+    private boolean isAdmin;
 
     public LoginResponse() {
     }
 
-    public LoginResponse(User user) {
-        this.user = user;
+    public LoginResponse(Long id, String email, String username, boolean isAdmin) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.isAdmin = isAdmin;
     }
 
-    public User getUser() {
-        return user;
+    public Long getId() {
+        return id;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
     }
 }
