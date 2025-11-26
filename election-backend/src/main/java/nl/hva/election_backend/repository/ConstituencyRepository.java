99813d13daabb8544458c6bf47c8c21069210f0c
Daan <daan.hoedjes@hva.nl>
2025-11-26 17:34:09 +0100

    refactor: add EntityGraph to fetch results and parties in findAllByYear

--- election-backend/src/main/java/nl/hva/election_backend/repository/ConstituencyRepository.java
+++ election-backend/src/main/java/nl/hva/election_backend/repository/ConstituencyRepository.java
@@ -3,6 +3,7 @@ package nl.hva.election_backend.repository;
 import nl.hva.election_backend.entity.ConstituencyEntity;
 import nl.hva.election_backend.entity.id.ConstituencyId;
 import nl.hva.election_backend.model.Constituency;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,5 +13,6 @@ import java.util.Set;
 public interface ConstituencyRepository extends JpaRepository<ConstituencyEntity, ConstituencyId> {
     Optional<ConstituencyEntity> findByNameAndYear(String name, int year);
 
-    List<ConstituencyEntity> findAllByYear(int year);
+    @EntityGraph(attributePaths = {"results", "results.party"})
+    Set<ConstituencyEntity> findAllByYear(int year);
 }
