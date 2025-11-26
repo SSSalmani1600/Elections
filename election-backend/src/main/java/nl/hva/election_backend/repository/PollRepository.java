29ed0cf4e0a0340acf8b9e5ebf7cc772a814b370
Afnaan Makhloufi <afnaan.makhloufi@hva.nl>
2025-11-26 23:58:32 +0100

    feat: add PollRepository extending JpaRepository for Poll entity

--- election-backend/src/main/java/nl/hva/election_backend/repository/PollRepository.java
+++ election-backend/src/main/java/nl/hva/election_backend/repository/PollRepository.java
@@ -1,4 +1,9 @@
 package nl.hva.election_backend.repository;
 
-public interface PollRepository {
+import nl.hva.election_backend.model.Poll;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PollRepository extends JpaRepository<Poll, UUID> {
 }
