c80b5919d40e2d9272b45ffc7d968b64e82d6308
Afnaan Makhloufi <afnaan.makhloufi@hva.nl>
2025-11-27 01:21:33 +0100

    feat(repository): add findTopByOrderByCreatedAtDesc() to PollRepository

--- election-backend/src/main/java/nl/hva/election_backend/repository/PollRepository.java
+++ election-backend/src/main/java/nl/hva/election_backend/repository/PollRepository.java
@@ -2,8 +2,10 @@ package nl.hva.election_backend.repository;
 
 import nl.hva.election_backend.model.Poll;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PollRepository extends JpaRepository<Poll, UUID> {
+
+    Optional<Poll> findTopByOrderByCreatedAtDesc();
 }
