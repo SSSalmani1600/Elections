c0628ea0cd159a2a7b4f5d9a6d0064d1ffffb770
Niek Goedemans <goedemn@ad.hva.nl>
2025-11-28 13:08:11 +0100

    feat(calculating-guide-results): update - PartyViewpoint entity

--- election-backend/src/main/java/nl/hva/election_backend/entity/PartyViewpointEntity.java
+++ election-backend/src/main/java/nl/hva/election_backend/entity/PartyViewpointEntity.java
@@ -3,9 +3,11 @@ package nl.hva.election_backend.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
-public class PartyViewpoint {
+@Table(name = "party_statement_positions")
+public class PartyViewpointEntity {
     @Id
     private Long id;
     @Column(name = "party_id")
@@ -14,9 +16,9 @@ public class PartyViewpoint {
     private Long statementId;
     private String position;
 
-    public PartyViewpoint(){}
+    public PartyViewpointEntity(){}
 
-    public PartyViewpoint(Long id, Long partyId, Long statementId, String position) {
+    public PartyViewpointEntity(Long id, Long partyId, Long statementId, String position) {
         this.id = id;
         this.partyId = partyId;
         this.statementId = statementId;
@@ -53,5 +55,21 @@ public class PartyViewpoint {
 
     public void setPosition(String position) {
         this.position = position;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PartyEntity that = (PartyEntity) o;
+
+        return statementId.equals(that.getPartyId());
     }
 }
