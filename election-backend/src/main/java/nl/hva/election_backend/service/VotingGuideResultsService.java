dfcb1537fd12cc2654c1900be29655590c83a2e3
Niek <niekgoedemans93@gmail.com>
2025-12-03 22:40:27 +0100

    feat(calculating-voting-guide-results): add - functionality for calculating results

--- election-backend/src/main/java/nl/hva/election_backend/service/VotingGuideResultsService.java
+++ election-backend/src/main/java/nl/hva/election_backend/service/VotingGuideResultsService.java
@@ -5,17 +5,26 @@ import nl.hva.election_backend.dto.VotingGuideRequestDto;
 import nl.hva.election_backend.dto.VotingGuideResponseDto;
 import nl.hva.election_backend.dto.VotingGuideResultDto;
 import nl.hva.election_backend.entity.PartyViewpointEntity;
+import nl.hva.election_backend.entity.VotingGuidePartyEntity;
+import nl.hva.election_backend.repository.VotingGuidePartyRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 @Service
 public class VotingGuideResultsService {
+    private final VotingGuidePartyRepository votingGuidePartyRepository;
+
+    public VotingGuideResultsService(VotingGuidePartyRepository votingGuidePartyRepository) {
+        this.votingGuidePartyRepository = votingGuidePartyRepository;
+    }
+
     public VotingGuideResponseDto calculate(VotingGuideRequestDto votingGuideAnswers, List<PartyViewpointEntity> partyViewpoints) {
         Set<VotingGuideResultDto> votingGuideResults = new HashSet<>();
         double totalAnswers = votingGuideAnswers.getVotingGuideAnswers().size();
         Map<Long, Map<Long, String>> partyViewpointsMap = new HashMap<>();
 
+//        Converting List to nested Map
         partyViewpoints.forEach(partyViewpoint -> {
             if (!partyViewpointsMap.containsKey(partyViewpoint.getPartyId())) {
                 partyViewpointsMap.put(partyViewpoint.getPartyId(), new HashMap<>());
@@ -26,5 +35,30 @@ public class VotingGuideResultsService {
                 innerMap.put(partyViewpoint.getStatementId(), partyViewpoint.getPosition());
             }
         });
+
+//        Looping through parties in map, comparing statements and checking if user answer is equals to party position
+        for (Map.Entry<Long, Map<Long, String>> entry : partyViewpointsMap.entrySet()) {
+            double score = 0;
+
+            for (VotingGuideAnswerDto votingGuideAnswer :  votingGuideAnswers.getVotingGuideAnswers()) {
+                Long userStatementId = votingGuideAnswer.getStatementId().longValue();
+                String userPosition = votingGuideAnswer.getAnswer();
+
+                Map<Long, String> innerMap = entry.getValue();
+                if (innerMap.containsKey(userStatementId)) {
+                    if (innerMap.get(userStatementId).equals(userPosition)) {
+                        score++;
+                    }
+                }
+            }
+            double percentage = (score / totalAnswers) * 100;
+
+            Optional<VotingGuidePartyEntity> party = votingGuidePartyRepository.findById(entry.getKey());
+            String partyName = party.isPresent() ? party.get().getName() : "Onbekende partij";
+
+            votingGuideResults.add(new VotingGuideResultDto(entry.getKey(), partyName, percentage));
+        }
+
+        return new VotingGuideResponseDto(votingGuideResults);
     }
 }
