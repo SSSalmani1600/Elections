255adc78f56508ccea5bf574a8a6e4862f7670a6
Niek <niekgoedemans93@gmail.com>
2025-12-06 22:05:52 +0100

    progressbar component: add - able to give a percentage to component

--- election-frontend/src/components/ProgressBar.vue
+++ election-frontend/src/components/ProgressBar.vue
@@ -4,15 +4,25 @@ import { computed } from 'vue'
 const props = defineProps({
   totalAnswered: Number,
   totalStatements: Number,
+  percentage: Number,
 })
 const calculatedWidthPercentage = computed(() => {
-  return props.totalAnswered && props.totalStatements ? (props.totalAnswered / props.totalStatements) * 100 : 0
+  if (props.percentage !== undefined) {
+    return props.percentage
+  } else {
+    return props.totalAnswered && props.totalStatements
+      ? (props.totalAnswered / props.totalStatements) * 100
+      : 0
+  }
 })
 </script>
 
 <template>
   <div class="w-full h-2 bg-white rounded-3xl">
-    <div class="h-2 rounded-3xl bg-primary transition-all duration-500" :style="{width: calculatedWidthPercentage + '%'}"></div>
+    <div
+      class="h-full rounded-3xl bg-primary transition-all duration-500"
+      :style="{ width: calculatedWidthPercentage + '%' }"
+    ></div>
   </div>
 </template>
 
