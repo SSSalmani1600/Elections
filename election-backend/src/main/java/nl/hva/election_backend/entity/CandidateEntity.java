2c70c18e19f41efab27a36c8980f25e09c4856fb
Afnaan Makhloufi <afnaan.makhloufi@hva.nl>
2025-12-09 22:01:52 +0100

    feat(entity): introduce CandidateEntity with XML fields and Party relation mapping

---
+++ election-backend/src/main/java/nl/hva/election_backend/entity/CandidateEntity.java
@@ -0,0 +1,89 @@
+package nl.hva.election_backend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "candidates")
+public class CandidateEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Dit is het ID uit de XML: "CandidateIdentifier Id"
+    @Column(name = "candidate_id")
+    private String candidateId;
+
+    private String firstName;
+    private String namePrefix;
+    private String lastName;
+    private String gender;
+    private String localityName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "party_id")
+    private PartyEntity party;
+
+
+    public CandidateEntity() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCandidateId() {
+        return candidateId;
+    }
+
+    public void setCandidateId(String candidateId) {
+        this.candidateId = candidateId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getNamePrefix() {
+        return namePrefix;
+    }
+
+    public void setNamePrefix(String namePrefix) {
+        this.namePrefix = namePrefix;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLocalityName() {
+        return localityName;
+    }
+
+    public void setLocalityName(String localityName) {
+        this.localityName = localityName;
+    }
+
+    public PartyEntity getParty() {
+        return party;
+    }
+
+    public void setParty(PartyEntity party) {
+        this.party = party;
+    }
+}
