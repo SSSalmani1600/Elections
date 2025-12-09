aa584d4d412b32aeaafe8d633706ba709efcbc57
Afnaan Makhloufi <afnaan.makhloufi@hva.nl>
2025-12-09 22:00:39 +0100

    feat: add CandidateRepository with query to fetch candidates by partyId

---
+++ election-backend/src/main/java/nl/hva/election_backend/repository/CandidateRepository.java
@@ -0,0 +1,12 @@
+package nl.hva.election_backend.repository;
+
+import nl.hva.election_backend.entity.CandidateEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CandidateRepository extends JpaRepository<CandidateEntity, String> {
+    List<CandidateEntity> findByParty_PartyIdOrderByCandidateIdAsc(String partyId);
+}
