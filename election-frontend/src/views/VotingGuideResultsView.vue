2eba08f71ab88caa1ba0f67ff4360a749dc84f78
Niek <niekgoedemans93@gmail.com>
2025-12-11 17:02:49 +0100

    feature(logged-in-user-voting-guide): add - fetching results from db when user is logged in

--- election-frontend/src/views/VotingGuideResultsView.vue
+++ election-frontend/src/views/VotingGuideResultsView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import type {VotingGuideResult, VotingGuideResultResponse} from '@/types/api.ts'
-import {onMounted, ref} from 'vue'
+import type { VotingGuideResult, VotingGuideResultResponse } from '@/types/api.ts'
+import { onMounted, ref } from 'vue'
 import ProgressBar from '@/components/ProgressBar.vue'
 import router from '@/router'
-import {Spinner} from "@/components/ui/spinner";
+import { Spinner } from '@/components/ui/spinner'
+import { useAuth } from '@/store/authStore.ts'
+import { getResults } from '@/services/VotingGuideResultsService.ts'
 
 const results = ref<VotingGuideResultResponse>({
   votingGuideResults: [],
@@ -12,27 +14,38 @@ const resultsTop3 = ref<VotingGuideResult[]>()
 const restOfResults = ref<VotingGuideResult[]>()
 const isLoadingResults = ref<boolean>(false)
 const resultsIsEmpty = ref<boolean>(false)
+const { user } = useAuth()
 
 onMounted(async () => {
   isLoadingResults.value = true
 
-  const resultsRaw = localStorage.getItem('voting_guide_results')
-  results.value = resultsRaw ? JSON.parse(resultsRaw) : null
+  try {
+    if (user) {
+      results.value = await getResults()
+      if (results.value.votingGuideResults !== undefined) localStorage.removeItem("voting_guide_answers")
+    } else {
+      const resultsRaw = localStorage.getItem('voting_guide_results')
+      results.value = resultsRaw ? JSON.parse(resultsRaw) : null
+    }
 
-  if (!resultsRaw && results.value == null) {
-    resultsIsEmpty.value = true
-    setTimeout(() => router.replace({name: "voting-guide"}), 3000)
-    return
+    // Send user back to voting guide when results are non-existing
+    if (results.value == null) {
+      resultsIsEmpty.value = true
+      setTimeout(() => router.replace({ name: 'voting-guide' }), 3000)
+      return
+    }
+
+    resultsTop3.value = results.value.votingGuideResults.slice(0, 3)
+    restOfResults.value = results.value.votingGuideResults.slice(3)
+    isLoadingResults.value = false
+  } catch(err: any) {
+    console.error(err.message)
   }
-
-  resultsTop3.value = results.value.votingGuideResults.slice(0, 3)
-  restOfResults.value = results.value.votingGuideResults.slice(3)
-  isLoadingResults.value = false
 })
 </script>
 
 <template>
-  <div class="flex flex-col items-center w-full my-10 gap-8 ">
+  <div class="flex flex-col items-center w-full my-10 gap-8">
     <div class="flex flex-col gap-2 items-center mx-4 text-center">
       <h1 class="text-4xl font-bold">Jouw Resultaten</h1>
       <p>Op basis van jouw antwoorden passen deze partijen het beste bij jou</p>
@@ -43,39 +56,41 @@ onMounted(async () => {
       class="bg-background p-10 rounded-[10px] absolute transform translate-[-50%] top-1/2 left-1/2"
     >
       <div class="flex items-center w-full gap-2">
-        <span class="text-red-400 font-bold text-xl flex items-center gap-2">ERROR <Spinner></Spinner></span>
+        <span class="text-red-400 font-bold text-xl flex items-center gap-2"
+          >ERROR <Spinner></Spinner
+        ></span>
         <div class="relative w-full flex">
           <span class="w-full h-2 rounded-lg bg-white"></span>
           <span class="progress w-full h-2 rounded-lg bg-primary absolute transition"></span>
         </div>
       </div>
-      <p class="text-lg">Er zijn geen bekende resultaten, u wordt terug gestuurd naar de
-        stemwijzer</p>
+      <p class="text-lg">
+        Er zijn geen bekende resultaten, u wordt terug gestuurd naar de stemwijzer
+      </p>
     </div>
     <div v-else class="flex flex-col gap-8 w-full items-center">
       <div
-        class="flex flex-col gap-5 justify-between w-[90%] md:w-[70%] lg:flex-row lg:h-[290px] lg:gap-10 2xl:w-[55%]">
+        class="flex flex-col gap-5 justify-between w-[90%] md:w-[70%] lg:flex-row lg:h-[290px] lg:gap-10 2xl:w-[55%]"
+      >
         <div
           v-for="(party, index) in resultsTop3"
           :key="party.partyId"
           class="flex flex-col w-full h-full bg-background rounded-lg p-3.5 justify-between shadow-lg gap-1 lg:gap-0"
         >
-          <div class="flex flex-col gap-2 ">
+          <div class="flex flex-col gap-2">
             <div class="flex items-center gap-2">
-
               <i
                 class="pi pi-crown"
                 :class="{
-              'text-yellow-400': index === 0,
-              'text-gray-400': index === 1,
-              'text-amber-600': index === 2,
-            }"
+                  'text-yellow-400': index === 0,
+                  'text-gray-400': index === 1,
+                  'text-amber-600': index === 2,
+                }"
               ></i>
               <span class="font-bold text-xl">{{ index + 1 }}e plaats</span>
             </div>
 
             <span class="font-bold text-xl mb-4 lg:mb-0">{{ party.partyName }}</span>
-
           </div>
           <div class="flex flex-col gap-1">
             <span class="text-xl font-bold">{{ Math.ceil(Number(party.percentage)) }}%</span>
