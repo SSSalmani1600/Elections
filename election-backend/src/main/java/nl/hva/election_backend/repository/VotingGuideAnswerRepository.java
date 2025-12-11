5afd1ad090c36ead53c69fcddeee44029b70080f
Niek <niekgoedemans93@gmail.com>
2025-12-11 11:56:06 +0100

    feature(logged-in-user-voting-guide): add - finding all answers by user id in db

--- election-backend/src/main/java/nl/hva/election_backend/repository/VotingGuideAnswerRepository.java
+++ election-backend/src/main/java/nl/hva/election_backend/repository/VotingGuideAnswerRepository.java
@@ -3,8 +3,12 @@ package nl.hva.election_backend.repository;
 import nl.hva.election_backend.entity.VotingGuideAnswerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VotingGuideAnswerRepository extends JpaRepository<VotingGuideAnswerEntity, Long> {
     boolean existsByUserId(Long id);
 
     void deleteAllByUserId(Long id);
+
+    List<VotingGuideAnswerEntity> findAllByUserId(Long userId);
 }
