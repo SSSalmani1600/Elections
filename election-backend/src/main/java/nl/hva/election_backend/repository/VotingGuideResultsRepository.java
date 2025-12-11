d053bbc194ac2e74d649f32a527c1a6fe81da9c8
Niek <niekgoedemans93@gmail.com>
2025-12-11 15:03:58 +0100

    feature(logged-in-user-voting-guide): removed unused method from repository

--- election-backend/src/main/java/nl/hva/election_backend/repository/VotingGuideResultsRepository.java
+++ election-backend/src/main/java/nl/hva/election_backend/repository/VotingGuideResultsRepository.java
@@ -1,15 +1,13 @@
 package nl.hva.election_backend.repository;
 
 import nl.hva.election_backend.entity.VotingGuideResultEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface VotingGuideResultsRepository {
+public interface VotingGuideResultsRepository extends JpaRepository<VotingGuideResultEntity, Long> {
     boolean existsByUserId(Long userId);
 
     void deleteAllByUserId(Long userId);
-
-    void saveAll(List<VotingGuideResultEntity> listOfResultEntities);
 }
