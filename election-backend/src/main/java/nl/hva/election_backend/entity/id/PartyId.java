b2e70167c137d771e38bf4c0b710eda78ab588fb
Afnaan Makhloufi <afnaan.makhloufi@hva.nl>
2025-12-12 23:10:31 +0100

    fix(jpa): make PartyId embeddable composite key

--- election-backend/src/main/java/nl/hva/election_backend/entity/id/PartyId.java
+++ election-backend/src/main/java/nl/hva/election_backend/entity/id/PartyId.java
@@ -1,19 +1,44 @@
 package nl.hva.election_backend.entity.id;
 
+import jakarta.persistence.Embeddable;
+import java.io.Serializable;
 import java.util.Objects;
 
-public class PartyId {
+@Embeddable
+public class PartyId implements Serializable {
+
     private int year;
     private String partyId;
 
-    public PartyId() {
+    public PartyId() {}
+
+    public PartyId(int year, String partyId) {
+        this.year = year;
+        this.partyId = partyId;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getPartyId() {
+        return partyId;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPartyId(String partyId) {
+        this.partyId = partyId;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        PartyId partyId1 = (PartyId) o;
-        return year == partyId1.year && Objects.equals(partyId, partyId1.partyId);
+        if (this == o) return true;
+        if (!(o instanceof PartyId)) return false;
+        PartyId that = (PartyId) o;
+        return year == that.year && Objects.equals(partyId, that.partyId);
     }
 
     @Override
