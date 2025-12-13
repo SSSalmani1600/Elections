fe21a545512ecb5424081aba9472e38a6dee199c
Afnaan Makhloufi <afnaan.makhloufi@hva.nl>
2025-12-13 23:04:58 +0100

    refactor(jpa): simplify PartyEntity primary key and add candidate relation

--- election-backend/src/main/java/nl/hva/election_backend/entity/PartyEntity.java
+++ election-backend/src/main/java/nl/hva/election_backend/entity/PartyEntity.java
@@ -2,59 +2,35 @@ package nl.hva.election_backend.entity;
 
 import jakarta.persistence.*;
 import nl.hva.election_backend.entity.id.PartyId;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@IdClass(PartyId.class)
 @Table(name = "parties")
 public class PartyEntity {
-
-    @Id
-    private int year;
 
     @Id
     @Column(name = "party_id")
     private String partyId;
 
-    @Column(name = "name", nullable = false)
+    @Column(nullable = false)
     private String name;
+
+    @Column(name = "year")
+    private int year;
+
+    @OneToMany(mappedBy = "party")
+    private Set<CandidateEntity> candidates = new HashSet<>();
 
     public PartyEntity() {}
 
     public PartyEntity(String name, int year, String partyId) {
+        this.partyId = partyId;
         this.name = name;
         this.year = year;
-        this.partyId = partyId;
     }
 
-    public String getPartyId() {
-        return partyId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-
-        if (o == null || getClass() != o.getClass()) return false;
-
-        PartyEntity that = (PartyEntity) o;
-
-        return name.equals(that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return name.hashCode();
-    }
+    public String getPartyId() { return partyId; }
+    public String getName() { return name; }
+    public int getYear() { return year; }
 }
