aebfd31932dbed0fe478d3ec2c2ca856b79c0537
Afnaan Makhloufi <afnaan.makhloufi@hva.nl>
2025-12-13 23:04:21 +0100

    feat(party-controller): add party detail endpoint and simplify controller

--- election-backend/src/main/java/nl/hva/election_backend/controller/parser/PartyController.java
+++ election-backend/src/main/java/nl/hva/election_backend/controller/parser/PartyController.java
@@ -1,30 +1,30 @@
 package nl.hva.election_backend.controller.parser;
 
-import nl.hva.election_backend.entity.PartyEntity;
-import nl.hva.election_backend.model.Party;
-import nl.hva.election_backend.service.DutchElectionService;
+import nl.hva.election_backend.dto.PartyDetail;
 import nl.hva.election_backend.service.PartyService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
+import nl.hva.election_backend.entity.PartyEntity;
 
 @RestController
 @RequestMapping("api/parties")
 public class PartyController {
-    private final PartyService partyService;
-    DutchElectionService electionService;
 
-    public PartyController(DutchElectionService electionService, PartyService partyService) {
-        this.electionService = electionService;
+    private final PartyService partyService;
+
+    public PartyController(PartyService partyService) {
         this.partyService = partyService;
     }
 
-    @GetMapping()
-    public Set<PartyEntity> readAffiliations() {
+    @GetMapping
+    public Set<PartyEntity> getAllParties() {
         return partyService.getParties();
     }
+
+    @GetMapping("/{partyId}")
+    public PartyDetail getDetail(@PathVariable String partyId) {
+        return partyService.getPartyDetail(partyId);
+    }
+
 }
