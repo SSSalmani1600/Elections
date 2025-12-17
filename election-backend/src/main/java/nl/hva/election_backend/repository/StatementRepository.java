31f3990e1484e0acf6c2ef1ea247fe610ea6d564
Niek <niekgoedemans93@gmail.com>
2025-12-17 13:36:35 +0100

    feature/voting-guide-home): add - repository method for finding only distinct categories

--- election-backend/src/main/java/nl/hva/election_backend/repository/StatementRepository.java
+++ election-backend/src/main/java/nl/hva/election_backend/repository/StatementRepository.java
@@ -2,9 +2,14 @@ package nl.hva.election_backend.repository;
 
 import nl.hva.election_backend.entity.StatementEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.LinkedHashSet;
+import java.util.List;
 
 public interface StatementRepository extends JpaRepository<StatementEntity, Long> {
     public LinkedHashSet<StatementEntity> findAllByOrderByIdAsc();
+
+    @Query("select distinct s.category from StatementEntity s")
+    List<String> findAllCategories();
 }
