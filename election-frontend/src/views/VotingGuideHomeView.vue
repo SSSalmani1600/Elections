871f4126955db237f5741c92e22d64627a600b6b
Niek <niekgoedemans93@gmail.com>
2025-12-17 19:55:49 +0100

    feature(voting-guide-home): add - loading skeleton for categories and parties, error message for categories en parties

--- election-frontend/src/views/VotingGuideHomeView.vue
+++ election-frontend/src/views/VotingGuideHomeView.vue
@@ -22,8 +22,15 @@ const array = ref<{ title: string; text: string }[]>([
 ])
 const categories = ref<string[]>()
 const parties = ref<VotingGuideParty[]>()
+const loadingCategories = ref<boolean>(false)
+const loadingParties = ref<boolean>(false)
+
+const categoriesHasError = ref<boolean>(false)
+const partiesHasError = ref<boolean>(false)
 
 onMounted(async () => {
+  loadingCategories.value = true
+  loadingParties.value = true
   try {
     categories.value = await getAllCategories()
   } catch (err: any) {
@@ -34,6 +41,9 @@ onMounted(async () => {
       detail: 'Er ging iets mis met het ophalen van de categorieen',
       life: 3000,
     })
+    categoriesHasError.value = true
+  } finally {
+    loadingCategories.value = false
   }
 
   try {
@@ -46,6 +56,9 @@ onMounted(async () => {
       detail: 'Er ging iets mis met het ophalen van de partijen',
       life: 3000,
     })
+    partiesHasError.value = true
+  } finally {
+    loadingParties.value = false
   }
 })
 </script>
@@ -78,26 +91,73 @@ onMounted(async () => {
     </div>
 
     <div class="flex flex-col items-center gap-3 w-[70%]">
-      <h4 class="font-bold text-2xl">De categorieen - {{ categories?.length }}</h4>
-      <div class="flex flex-wrap gap-2 justify-center">
-        <span
-          v-for="category in categories"
-          :key="category"
-          class="bg-background p-2 px-4 shadow-lg rounded-lg"
-          >{{ category }}</span
-        >
+      <div class="flex items-center gap-2 font-bold text-2xl">
+        <h4>De categorieen</h4>
+        <span v-if="categories && categories.length > 0">- {{ categories?.length }}</span>
       </div>
+      <div v-if="!categoriesHasError">
+        <!-- Skeleton -->
+        <div v-if="loadingCategories" class="flex flex-wrap gap-2 justify-center">
+          <span v-for="n in 14" :key="n" class="skeleton-text h-8 w-24 rounded-lg"></span>
+        </div>
+
+        <!-- Content -->
+        <div v-else class="flex flex-wrap gap-2 justify-center">
+          <span
+            v-for="category in categories"
+            :key="category"
+            class="bg-background p-2 px-4 shadow-lg rounded-lg"
+          >
+            {{ category }}
+          </span>
+        </div>
+      </div>
+
+      <span v-else class="bg-background p-4 px-6 rounded-lg text-primary"
+        >Er konden geen categorieen gevonden worden!</span
+      >
     </div>
 
     <div class="w-[80%] flex flex-col items-center gap-3">
-      <h4 class="font-bold text-2xl">Deelnemende partijen - {{ parties?.length }}</h4>
-      <div class="flex flex-wrap gap-2 justify-center">
-        <span v-for="party in parties" :key="party.id" class="bg-background p-2 px-4 rounded-lg">{{
-          party.name
-        }}</span>
+      <div class="flex items-center gap-2 font-bold text-2xl">
+        <h4>Deelnemende partijen</h4>
+        <span v-if="parties && parties.length > 0">- {{ parties?.length }}</span>
       </div>
+      <div v-if="!partiesHasError">
+        <!-- Skeleton -->
+        <div v-if="loadingParties" class="flex flex-wrap gap-2 justify-center">
+          <span v-for="n in 14" :key="n" class="skeleton-text h-8 w-32 rounded-lg"></span>
+        </div>
+
+        <!-- Content -->
+        <div v-else class="flex flex-wrap gap-2 justify-center">
+          <span v-for="party in parties" :key="party.id" class="bg-background p-2 px-4 rounded-lg">
+            {{ party.name }}
+          </span>
+        </div>
+      </div>
+
+      <span v-else class="bg-background p-4 px-6 rounded-lg text-primary"
+        >Er konden geen partijen gevonden worden!</span
+      >
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.skeleton-img,
+.skeleton-text {
+  background: linear-gradient(90deg, #3e3e3e 0%, #555 50%, #3e3e3e 100%);
+  background-size: 200% 100%;
+  animation: skeleton-loading 1.2s infinite;
+}
+
+@keyframes skeleton-loading {
+  0% {
+    background-position: 200% 0;
+  }
+  100% {
+    background-position: -200% 0;
+  }
+}
+</style>
