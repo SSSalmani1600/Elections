b8073f2efe2ee62a75c0c6a6e36999d79ef752f6
Nabil Salmani <nabil.salmani@hva.nl>
2025-12-24 19:34:51 +0100

    feat: add validation constraints to UpdateDiscussionRequest for userId, title, and body

--- election-backend/src/main/java/nl/hva/election_backend/dto/UpdateDiscussionRequest.java
+++ election-backend/src/main/java/nl/hva/election_backend/dto/UpdateDiscussionRequest.java
@@ -1,4 +1,17 @@
 package nl.hva.election_backend.dto;
 
-public record UpdateDiscussionRequest() {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public record UpdateDiscussionRequest(
+
+        @NotBlank
+        String userId,
+        @NotBlank
+        @Min(6)
+        @Max(value=50, message = "Veels te l;amg...")
+        String title,
+        String body
+) {
 }
