2c2cbdb11d92cdedc355b82fa86dddc2aaf63cae
Nabil Salmani <nabil.salmani@hva.nl>
2026-01-04 17:28:29 +0100

    refactor: update UpdateDiscussionRequest to a class with getter and setter methods; add UpdateReactionRequest class for handling user reactions

--- election-backend/src/main/java/nl/hva/election_backend/dto/UpdateDiscussionRequest.java
+++ election-backend/src/main/java/nl/hva/election_backend/dto/UpdateDiscussionRequest.java
@@ -1,19 +1,45 @@
 package nl.hva.election_backend.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record UpdateDiscussionRequest(
+public class UpdateDiscussionRequest {
 
-        @NotBlank
-        String userId,
+    @NotNull(message = "userId is verplicht")
+    private Long userId;
 
-        @NotBlank(message = "Titel is verplicht")
-        @Size(min = 5, max = 255, message = "Titel moet tussen de 5 en 255 karakters zijn")
-        String title,
+    @NotBlank(message = "Titel is verplicht")
+    @Size(min = 5, max = 255, message = "Titel moet tussen de 5 en 255 karakters zijn")
+    private String title;
 
-        @NotBlank(message = "Inhoud is verplicht")
-        @Size(min = 10, message = "Inhoud moet minimaal 10 karakters bevatten")
-        String body
-) {
+    @NotBlank(message = "Inhoud is verplicht")
+    @Size(min = 10, message = "Inhoud moet minimaal 10 karakters bevatten")
+    private String body;
+
+    public UpdateDiscussionRequest() {}
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
