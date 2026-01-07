d951edc0762807aaf5bcb02fdcca1e8b2d228dd4
Afnaan Makhloufi <afnaan.makhloufi@hva.nl>
2026-01-07 13:40:03 +0100

    refactor: align PollVoteRepository with Poll entity relations

--- election-backend/src/main/java/nl/hva/election_backend/repository/PollVoteRepository.java
+++ election-backend/src/main/java/nl/hva/election_backend/repository/PollVoteRepository.java
@@ -1,5 +1,6 @@
 package nl.hva.election_backend.repository;
 
+import nl.hva.election_backend.model.Poll;
 import nl.hva.election_backend.model.PollVote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,6 +8,6 @@ import java.util.List;
 import java.util.UUID;
 
 public interface PollVoteRepository extends JpaRepository<PollVote, UUID> {
-    List<PollVote> findByPollId(UUID pollId);
-    boolean existsByPollIdAndUserId(UUID pollId, Long userId);
+    boolean existsByPollAndUserId(Poll poll, Long userId);
+    List<PollVote> findByPoll(Poll poll);
 }
