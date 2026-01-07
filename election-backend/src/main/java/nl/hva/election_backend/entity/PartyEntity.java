73d8a1fab47d6fd42f574ef6ad88b52c942e9adc
Afnaan Makhloufi <afnaan.makhloufi@hva.nl>
2026-01-07 16:38:17 +0100

    change back

--- election-backend/src/main/java/nl/hva/election_backend/entity/PartyEntity.java
+++ election-backend/src/main/java/nl/hva/election_backend/entity/PartyEntity.java
@@ -2,32 +2,28 @@ package nl.hva.election_backend.entity;
 
 import jakarta.persistence.*;
 import nl.hva.election_backend.entity.id.PartyId;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
+@IdClass(PartyId.class)
 @Table(name = "parties")
 public class PartyEntity {
+
+    @Id
+    private int year;
 
     @Id
     @Column(name = "party_id")
     private String partyId;
 
-    @Column(nullable = false)
+    @Column(name = "name", nullable = false)
     private String name;
-
-    @Column(name = "year")
-    private int year;
-
-    @OneToMany(mappedBy = "party")
-    private Set<CandidateEntity> candidates = new HashSet<>();
 
     public PartyEntity() {}
 
     public PartyEntity(String name, int year, String partyId) {
-        this.partyId = partyId;
         this.name = name;
         this.year = year;
+        this.partyId = partyId;
     }
 
     public String getPartyId() {
--- election-backend/src/main/java/nl/hva/election_backend/entity/id/PartyId.java
+++ election-backend/src/main/java/nl/hva/election_backend/entity/id/PartyId.java
@@ -1,44 +1,19 @@
 package nl.hva.election_backend.entity.id;
 
-import jakarta.persistence.Embeddable;
-import java.io.Serializable;
 import java.util.Objects;
 
-@Embeddable
-public class PartyId implements Serializable {
-
+public class PartyId {
     private int year;
     private String partyId;
 
-    public PartyId() {}
-
-    public PartyId(int year, String partyId) {
-        this.year = year;
-        this.partyId = partyId;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public String getPartyId() {
-        return partyId;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public void setPartyId(String partyId) {
-        this.partyId = partyId;
+    public PartyId() {
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PartyId)) return false;
-        PartyId that = (PartyId) o;
-        return year == that.year && Objects.equals(partyId, that.partyId);
+        if (o == null || getClass() != o.getClass()) return false;
+        PartyId partyId1 = (PartyId) o;
+        return year == partyId1.year && Objects.equals(partyId, partyId1.partyId);
     }
 
     @Override
